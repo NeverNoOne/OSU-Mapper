b17392a3a9f28885c1534df30987fd4815ad502d
NeverNoOne <max.38@outlook.de>
2024-12-22 19:27:33 +0100

    optimized label generation

--- Pattern-Generation/osu_dataset.py
+++ Pattern-Generation/osu_dataset.py
@@ -4,10 +4,9 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from typing import Any
 import osu
+from audio.analysis import AudioAnalyser
 
-#TODO self.BeatMaps should be the output fromat --> what output format do I want/need from the Transformer
-#idea: ndarray[onset_times,  onset_strenghts] and ndarray[noteplacement]
-#TODO testing if it works
+#TODO adding difficulty to labels and modify __getitem__ for it
 class OsuBeatmapDataset(Dataset):
     def __init__(self, map_dir, transform=None, target_transform=None, count_mapsets=None) -> None:
         """
@@ -20,9 +19,9 @@ class OsuBeatmapDataset(Dataset):
         target_transform : callable, optional
             Optional transform to apply to target labels.
         """
-        maps = osu.BeatMap.getMaps_from_Dir(map_dir, count_mapsets=count_mapsets)
+        maps = [x for x in osu.BeatMap.getMaps_from_Dir(map_dir, count_mapsets=count_mapsets) if x.General.mode == osu.Mode_Type.osu]
         self.audio_labels = self.__generate_labels__(maps)
-        self.BeatMaps = maps
+        self.note_placements = [[int(y.time) for y in x.HitObjects] for x in maps]
         self.transform = transform
         self.target_transform = target_transform
 
@@ -31,7 +30,7 @@ class OsuBeatmapDataset(Dataset):
     
     def __getitem__(self, idx:int):
         _, onsets = self.audio_labels[idx]
-        note_placement = [int(x.time) for x in self.BeatMaps[idx].HitObjects]
+        note_placement = self.note_placements[idx]
 
         if self.transform:
             onsets = self.transform(onsets)
@@ -42,38 +41,45 @@ class OsuBeatmapDataset(Dataset):
 
     def __generate_labels__(self, maps:list[osu.BeatMap]):
         '''
-        returns a tuple of [bpm, another tuple[onset_strenghts, onset_times]]
+        returns a tuple of [bpm, ndarray[onset_strenghts, onset_times]]
         '''
         values:list[tuple[Any, np.ndarray]] = []
         values.clear()
         print("generating audio labels...")
         count = 0
+        AA:AudioAnalyser
+        oldAudioFileName:str = ''
+        oldTitle:str = ''
         for map in maps:
-            AA = map.General.GetAudioAnalyser()
-            bpm, _ = AA.Get_Beattrack()
-            onset_strenghts, onset_times = AA.Get_Onsets()
-            values.append((bpm, np.vstack((onset_strenghts, onset_times))))
-            del AA
+            if map.General.AudioFilename != oldAudioFileName or map.Metadata.Title != oldTitle:
+                oldAudioFileName = map.General.AudioFilename
+                oldTitle = map.Metadata.Title
+                AA = map.General.GetAudioAnalyser()
+                bpm, _ = AA.Get_Beattrack()
+                onset_strenghts, onset_times = AA.Get_Onsets()
+            values.append((bpm, np.vstack((onset_strenghts, onset_times)))) # type: ignore
+            #del AA
             count += 1
             print(f'{100 * count/len(maps):0.2f}%')
+            print ("\033[A\033[A")
         print("audio labels generated")
         return values
     
 if __name__ == '__main__':
-    # import tracemalloc
-    # tracemalloc.start()
-    # ds = OsuBeatmapDataset('Maps')
-    # dl = DataLoader(ds)
-    # current, peak = tracemalloc.get_traced_memory()
-    # print(f"{current / 1024 / 1024:.2f}mb")
-    # print(f"{peak / 1024 / 1024:.2f}mb")
-    # print(f'generated {len(ds)} audio labels')
-    # tracemalloc.stop()
-    lbm = osu.BeatMap.getMaps_from_MapDir('Maps/839864 S3RL - Catchit (Radio Edit)')
-    bm = lbm[0]
-    AA = bm.General.GetAudioAnalyser()
-    onset_strenghts, onset_times = AA.Get_Onsets()
-    print(onset_strenghts.shape)
-    print(onset_times.shape)
-    combined = np.vstack((onset_strenghts, onset_times))
-    print(combined.shape)
+    import tracemalloc
+    tracemalloc.start()
+    ds = OsuBeatmapDataset('Maps')
+    dl = DataLoader(ds)
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"{current / 1024 / 1024:.2f}mb")
+    print(f"{peak / 1024 / 1024:.2f}mb")
+    print(f'generated {len(ds)} audio labels')
+    tracemalloc.stop()
+    # lbm = osu.BeatMap.getMaps_from_MapDir('Maps/839864 S3RL - Catchit (Radio Edit)')
+    # bm = lbm[0]
+    # AA = bm.General.GetAudioAnalyser()
+    # onset_strenghts, onset_times = AA.Get_Onsets()
+    # print(onset_strenghts.shape)
+    # print(onset_times.shape)
+    # combined = np.vstack((onset_strenghts, onset_times))
+    # print(combined.shape)
